a77fdae972f668574b3586c173dafdbf4a4529d5
DarcyHu999 <huqy1021_cd@outlook.com>
2022-07-01 18:30:32 +1000

    add a comment

--- src/main/java/com/example/dataprep/datasource/PostgresDatasource.java
+++ src/main/java/com/example/dataprep/datasource/PostgresDatasource.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+//real database
 @Configuration
 public class PostgresDatasource {
     @ConfigurationProperties("app.datasource")
