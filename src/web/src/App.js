9bfef03f07ffa98d0a4ac6a2e1be05a6017a921a
Waoecher <630225992@qq.com>
2022-07-18 00:35:42 +1000

    summary login

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import React from 'react';
 import Search from "./page/Search";
+import { BrowserRouter ,Router,Routes, Link,Route} from "react-router-dom";
+import Summary from './page/Summary'
+import LogIn from './page/logIn'
+import SearchPage from './page/Search'
 function App() {
-  return (
-    <div className="App">
-        <Search></Search>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/summary" element={<Summary />} />
+                    <Route path="/" element={<LogIn />} />
+                    <Route path="/search" element={< SearchPage/>} />
+
+                </Routes>
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
 export default App;
