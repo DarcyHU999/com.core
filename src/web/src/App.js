427d76ac93b6856b94d09ad659b96c7e251d04fd
Carrey Jin <carreyjin@CarreydeMacBook-Pro.local>
2022-07-31 16:53:41 +0800

    carrey 7.31

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter ,Router,Routes, Link,Route} from "react-router-dom";
 import Summary from './page/Summary'
 import LogIn from './page/logIn'
 import SearchPage from './page/Search'
+import Register from './page/Register';
 function App() {
     return (
         <div className="App">
@@ -13,7 +14,7 @@ function App() {
                     <Route path="/summary" element={<Summary />} />
                     <Route path="/" element={<LogIn />} />
                     <Route path="/search" element={< SearchPage/>} />
-
+                    <Route path="/Register" element={< Register/>} />
                 </Routes>
             </BrowserRouter>
 
---
+++ src/web/src/page/Register.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import { useState } from "react";
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import PropTypes from 'prop-types';
+import { BrowserRouter, Router, Link, Route,withRouter,useHistory } from "react-router-dom";
+import { Button, Checkbox, Form, Input, notification } from 'antd';
+import axios from 'axios';
+//import './style.css';
+import SkeletonInput from 'antd/lib/skeleton/Input';
+
+const Register = props => {
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+        submit_user(values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    const [Inputs, setInput] = useState("");
+
+    function getinput(event) {
+        setInput(previousState => {
+            console.log({ Inputs });
+            return { Inputs: event.target.value }
+        })
+    }
+    function submit_user(data) {
+        // debugger;
+    //     fetch('http://localhost:8080/api/login', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Accept': 'application/json'
+
+    //         },
+    //         body: JSON.stringify({
+    //             username: data.username,
+    //             password: data.password
+    //         })
+    //     }).then((response) => {
+    //         if (response.code == 932361) {
+    //             props.history.push('/Search');
+    //         }
+    //         if (response.code == 932360) {
+    //             notification.error({
+    //                 message: response.msg,
+    //             })
+    //         }
+    //         console.log(response);
+    //     })
+    //         .catch(function (error) {
+    //             console.log(error);
+    //         })
+    // }
+    axios.post('http://127.0.0.1:8080/api/user', {
+        username: data.username,
+        password: data.password
+    })
+        .then(function (response) {
+            if (response.data.code == 932311) {
+                //props.history.push('/Search');
+                //跳转回login页面
+                window.location.href="/Search";
+                
+                // notification.success({
+                //     message: response.data.msg
+                // })
+        
+            }
+            if (response.data.code == 932310) {
+                notification.error({
+                    message : response.data.msg
+                    
+                })
+                //alert( response.data.msg);
+            }
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    return (
+        <div className = 'box1'>   
+        <Form
+            name="normal_register"    //change
+            className="register-form"     //change
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+        >
+            <Form.Item
+
+                name="username"
+                className = 'cssusername1'
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Username!',
+                    },
+                ]}
+            >
+                <Input
+                    prefix={<UserOutlined className="site-form-item-icon" />}
+                    onChange={getinput}
+                    placeholder="Username"
+                    allowClear={true}
+                />
+            </Form.Item>
+            <Form.Item
+                name="password"
+                className = 'csspass1'
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Password!',
+                    },
+                ]}
+            >
+                <Input
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    onChange={getinput}
+                    placeholder="Password"
+                    allowClear={true}
+                    
+                />
+            </Form.Item>
+            {/* <Form.Item className = 'remember'>
+            
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+            </Form.Item> */}
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="register-form-button">
+                    Register
+                </Button>
+            </Form.Item>
+        </Form>
+        </div>
+    )
+}   // end of Regist
+export default Register;
