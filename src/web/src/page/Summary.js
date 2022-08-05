7259b2b720405f350a16f7677be01a4d503324f6
Waoecher <630225992@qq.com>
2022-08-05 15:05:32 +1000

    back

--- src/web/src/page/Summary.js
+++ src/web/src/page/Summary.js
@@ -5,34 +5,33 @@ import ReactEcharts from 'echarts-for-react';
 import { Col, Row } from 'antd';
 import {Button, Card,Tag} from 'antd';
 import { DownloadOutlined } from '@ant-design/icons';
-import iojson from 'iojson'
+import iojson from 'iojson';
+import ReactJson from 'react-json-view';
 const Summary = props => {
     let data=useLocation().state
     data=JSON.parse(data)
     data=data[0]
     console.log(data)
     var freq=data["keyFreq"]
-    if(data["usualPort"]){
-        var port=data["usualPort"][0]
-    }
+
     var notTag=data["numOfNotIncludeTagsGroup"]
     var tag=data["numOfIncludeTagsGroup"]
     var groupCase=data["groupCase"]
-    var ipRange=""
-    var fromPort = null
-    var ipProtocol = null
-    if(groupCase['IpPermissions'] && groupCase['IpPermissions'][0] && groupCase['IpPermissions'][0]['IpRanges']) {
-        for (let i = 0; i < groupCase['IpPermissions'][0]['IpRanges'].length; i++) {
-            if (i != 0) {
-                ipRange = ipRange + ", "
-            }
-            ipRange = ipRange + groupCase['IpPermissions'][0]['IpRanges'][i]['CidrIp']
-
-
-        }
-        fromPort = groupCase['IpPermissions'][0]['FromPort']
-        ipProtocol = groupCase['IpPermissions'][0]['IpProtocol']
-    }
+    // var ipRange=""
+    // var fromPort = null
+    // var ipProtocol = null
+    // if(groupCase['IpPermissions'] && groupCase['IpPermissions'][0] && groupCase['IpPermissions'][0]['IpRanges']) {
+    //     for (let i = 0; i < groupCase['IpPermissions'][0]['IpRanges'].length; i++) {
+    //         if (i != 0) {
+    //             ipRange = ipRange + ", "
+    //         }
+    //         ipRange = ipRange + groupCase['IpPermissions'][0]['IpRanges'][i]['CidrIp']
+    //
+    //
+    //     }
+    //     fromPort = groupCase['IpPermissions'][0]['FromPort']
+    //     ipProtocol = groupCase['IpPermissions'][0]['IpProtocol']
+    // }
 
 
     const outJson =() => {
@@ -104,18 +103,19 @@ const Summary = props => {
                         <div style={{fontSize:"2rem",fontWeight:"900",textAlign:"center"}}>
                             Group Case
                         </div>
-                        <Tag color="gold" style={{fontSize:"1.5rem",fontWeight:"900",height:"28px"}}>Description </Tag><b style={{fontSize:"1.5rem",fontWeight:"900"}}> <b style={{fontSize:"1.2rem",fontWeight:"500",color:"black"}}>{groupCase['Description']}</b></b>
-                        <br/>
-                        <Tag color="gold" style={{fontSize:"1.5rem",fontWeight:"900",height:"28px"}}>Group ID </Tag><b style={{fontSize:"1.5rem",fontWeight:"900"}}> <b style={{fontSize:"1.2rem",fontWeight:"500",color:"black"}}>{groupCase['GroupId']}</b></b>
-                        <br/>
-                        <Tag color="gold" style={{fontSize:"1.5rem",fontWeight:"900",height:"28px"}}>Group Name </Tag><b style={{fontSize:"1.5rem",fontWeight:"900"}}> <b style={{fontSize:"1.2rem",fontWeight:"500",color:"black"}}>{groupCase['GroupName']}</b></b>
-                        <br/>
-                        <Tag color="gold" style={{fontSize:"1.5rem",fontWeight:"900",height:"28px"}}>From Port </Tag><b style={{fontSize:"1.5rem",fontWeight:"900"}}> <b style={{fontSize:"1.2rem",fontWeight:"500",color:"black"}}>{fromPort}</b></b>
-                        <br/>
-                        <Tag color="gold" style={{fontSize:"1.5rem",fontWeight:"900",height:"28px"}}>Ip Protocol  </Tag><b style={{fontSize:"1.5rem",fontWeight:"900"}}> <b style={{fontSize:"1.2rem",fontWeight:"500",color:"black"}}>{ipProtocol}</b></b>
-                        <br/>
-                        <Tag color="gold" style={{fontSize:"1.5rem",fontWeight:"900",height:"28px"}}>Ip Ranges </Tag><b style={{fontSize:"1.5rem",fontWeight:"900"}}> <b style={{fontSize:"1.2rem",fontWeight:"500",color:"black"}}>{ipRange}</b></b>
-                        <br/>
+                        {/*<Tag color="gold" style={{fontSize:"1.5rem",fontWeight:"900",height:"28px"}}>Description </Tag><b style={{fontSize:"1.5rem",fontWeight:"900"}}> <b style={{fontSize:"1.2rem",fontWeight:"500",color:"black"}}>{groupCase['Description']}</b></b>*/}
+                        {/*<br/>*/}
+                        {/*<Tag color="gold" style={{fontSize:"1.5rem",fontWeight:"900",height:"28px"}}>Group ID </Tag><b style={{fontSize:"1.5rem",fontWeight:"900"}}> <b style={{fontSize:"1.2rem",fontWeight:"500",color:"black"}}>{groupCase['GroupId']}</b></b>*/}
+                        {/*<br/>*/}
+                        {/*<Tag color="gold" style={{fontSize:"1.5rem",fontWeight:"900",height:"28px"}}>Group Name </Tag><b style={{fontSize:"1.5rem",fontWeight:"900"}}> <b style={{fontSize:"1.2rem",fontWeight:"500",color:"black"}}>{groupCase['GroupName']}</b></b>*/}
+                        {/*<br/>*/}
+                        {/*<Tag color="gold" style={{fontSize:"1.5rem",fontWeight:"900",height:"28px"}}>From Port </Tag><b style={{fontSize:"1.5rem",fontWeight:"900"}}> <b style={{fontSize:"1.2rem",fontWeight:"500",color:"black"}}>{fromPort}</b></b>*/}
+                        {/*<br/>*/}
+                        {/*<Tag color="gold" style={{fontSize:"1.5rem",fontWeight:"900",height:"28px"}}>Ip Protocol  </Tag><b style={{fontSize:"1.5rem",fontWeight:"900"}}> <b style={{fontSize:"1.2rem",fontWeight:"500",color:"black"}}>{ipProtocol}</b></b>*/}
+                        {/*<br/>*/}
+                        {/*<Tag color="gold" style={{fontSize:"1.5rem",fontWeight:"900",height:"28px"}}>Ip Ranges </Tag><b style={{fontSize:"1.5rem",fontWeight:"900"}}> <b style={{fontSize:"1.2rem",fontWeight:"500",color:"black"}}>{ipRange}</b></b>*/}
+                        {/*<br/>*/}
+                        <ReactJson src={groupCase} style={{fontSize:"1.5rem"}}/>
 
                     </div>
                 </Col>
